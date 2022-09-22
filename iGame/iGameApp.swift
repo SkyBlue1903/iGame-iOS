ef0572a4b7ac960f2acdc47322fbd915af21c712
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-09-22 07:00:45 +0700

    Initial Commit

---
+++ iGame/iGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  iGameApp.swift
+//  iGame
+//
+//  Created by Erlangga Anugrah Arifin on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
