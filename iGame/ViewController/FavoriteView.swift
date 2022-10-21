7b152ebcd2cc3bec5c0f5f5fe0946aee1c7a0f47
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-10-21 09:36:06 +0700

    FINALLY !!

--- iGame/ViewController/FavoriteView.swift
+++ iGame/ViewController/FavoriteView.swift
@@ -27,36 +27,46 @@ struct FavoriteView: View {
       } else {
         List {
           ForEach(games) { game in
-//                    NavigationLink(destination: GameDetailView(game: Game(
-//                            id: Int(game.id),
-//                            name: game.name ?? "Untitled",
-//                            released: game.released ?? "Unknown",
-//                            rating: game.rating ?? 0.0,
-//                            background_image: game.background_image ?? "",
-//                            description: game.game_description ?? "Lorem Ipsum",
-//                            website: game.website ?? "",
-//                            genres: game.genres as! [String] ?? ["Unknown"],
-//                            publishers: game.publishers as! [String] ?? ["Unknown"],
-//                            developers: game.developers as! [String] ?? ["Unknown"],
-//                            platforms: game.platforms as! [String] ?? ["Unknown"],
-//                            tags: game.tags as! [String] ?? ["Unknown"],
-//                            ratings: game.ratings as! [String] ?? ["Unknown"],
-//                            screenshots: game.screenshots as! [String] ?? ["Unknown"]
-//                    ))) {
-//                      let _ = print("Game name: \(game.name ?? "No name")")
-//
-//
-//                    }
-            let _ = print("Game Tags:", game.tags as! [String] ?? "No tags")
-            VStack {
-              Text(game.name!)
-                      .font(.body)
-                      .fontWeight(.heavy)
-              ForEach(game.publishers as! [String], id: \.self) { tag in
-                Text(tag)
-                        .multilineTextAlignment(.leading)
-                        .font(.caption)
-
+            NavigationLink(destination: GameDetailView(game: Game(
+                    id: Int(game.id),
+                    name: game.name ?? "Untitled",
+                    released: game.released ?? "Unknown",
+                    rating: game.rating ?? 0.0,
+                    background_image: game.background_image ?? "",
+                    description: game.game_description ?? "Lorem Ipsum",
+                    website: game.website ?? "",
+                    genres: game.genres as! [String] ?? ["Unknown"],
+                    publishers: game.publishers as! [String] ?? ["Unknown"],
+                    developers: game.developers as! [String] ?? ["Unknown"],
+                    platforms: game.platforms as! [String] ?? ["Unknown"],
+                    tags: game.tags as! [String] ?? ["Unknown"],
+                    ratings: game.ratings as! [String] ?? ["Unknown"],
+                    screenshots: game.screenshots as! [String] ?? ["Unknown"]
+            ))) {
+              let _ = print("Game Name: \(game.name ?? "No name")")
+              let _ = print("Game Tags:", game.tags as! [String] ?? "No tags")
+              HStack {
+                WebImage(url: URL(string: game.background_image ?? ""))
+                        .resizable()
+                        .placeholder {
+                          Rectangle().foregroundColor(.gray)
+                        }
+                        .indicator(.activity)
+                        .transition(.fade(duration: 0.5))
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(10)
+                VStack(alignment: .leading) {
+                  Text(game.name ?? "Untitled")
+                          .font(.title2)
+                          .fontWeight(.bold)
+                  Text(game.released!.prefix(4))
+                          .font(.subheadline)
+                          .foregroundColor(.gray)
+                  Text("Rating: \(game.rating ?? 0.0)")
+                          .font(.subheadline)
+                          .foregroundColor(.gray)
+                }
               }
             }
 
